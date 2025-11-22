d16b57fdfb190a509692d658ed64f5174d9e27dd
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-11-23 02:25:52 +0500

    Task6

---
+++ Tyuiu.MatveevaAA.Sprint4.Task6.V24.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.MatveevaAA.Sprint4.Task6.V24.Lib;
+
+namespace Tyuiu.MatveevaAA.Sprint4.Task6.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+      
+    }
+}
