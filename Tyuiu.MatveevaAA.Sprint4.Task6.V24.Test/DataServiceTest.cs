353f31f525ef875f521209642232ab61bb780355
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-11-23 02:34:28 +0500

    Task6

--- Tyuiu.MatveevaAA.Sprint4.Task6.V24.Test/DataServiceTest.cs
+++ Tyuiu.MatveevaAA.Sprint4.Task6.V24.Test/DataServiceTest.cs
@@ -6,6 +6,15 @@ namespace Tyuiu.MatveevaAA.Sprint4.Task6.V24.Test
     public class DataServiceTest
     {
         [TestMethod]
-      
+        public void Calculate()
+        {
+            DataService ds = new DataService();
+            string[] array = { "Êîìïüþòåð", "Òåëåôîí", "Ïëàíøåò", "Ïðèíòåð", "Ñêàíåð", "Ìîíèòîð", "Êëàâèàòóðà" };
+            string[] wait = { "Êîìïüþòåð", "Êëàâèàòóðà" };
+            string[] res = ds.Calculate(array);
+            CollectionAssert.AreEqual(wait, res);
+
+
+        }
     }
 }
