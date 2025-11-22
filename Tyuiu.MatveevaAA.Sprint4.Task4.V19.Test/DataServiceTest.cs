85467f6392e5650a1ceecc8f7854fba05ce9be51
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-11-23 01:59:38 +0500

    Task4

---
+++ Tyuiu.MatveevaAA.Sprint4.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MatveevaAA.Sprint4.Task4.V19.Lib;
+
+namespace Tyuiu.MatveevaAA.Sprint4.Task4.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void MaxInThirdRow()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[5, 5]  { { 5, 7, 6, 6, 5 },
+                                           { 5, 5, 5, 4, 6 },
+                                           { 8, 5, 7, 4, 7 },
+                                           { 7, 6, 7, 4, 7 },
+                                           { 5, 8, 7, 8, 8 } };
+            int res = ds.Calculate(mas2);
+            int wait = 8;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MatveevaAA.Sprint4.Task4.V19/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.MatveevaAA.Sprint4.Task4.V19.Lib;
+
+DataService ds = new DataService();
+int rows = 5;
+int columns = 5;
+int[,] mtrx = new int[rows, columns];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.WriteLine($"napishi {i}, {j} element");
+        mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+Console.WriteLine("\nMassive:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{mtrx[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+int res = ds.Calculate(mtrx);
+Console.WriteLine("summa nechetnih elementov: " + res);
+Console.ReadKey();
