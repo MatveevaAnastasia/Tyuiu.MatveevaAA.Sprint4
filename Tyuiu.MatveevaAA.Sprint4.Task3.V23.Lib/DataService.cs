270951c42619db6fddbed0073d8fc06fd4ab51b0
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-11-23 01:53:55 +0500

    Ефыл3

---
+++ Tyuiu.MatveevaAA.Sprint4.Task3.V23.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MatveevaAA.Sprint4.Task3.V23.Lib
+{
+    public class DataService : ISprint4Task3V23
+    {
+        public int Calculate(int[,] array)
+        {
+            int sum = 0;
+
+            foreach (var item in array)
+            {
+                if (item % 2 == 0)
+                {
+                    sum++;
+                }
+            }
+            return sum;
+        }
+    }
+}
