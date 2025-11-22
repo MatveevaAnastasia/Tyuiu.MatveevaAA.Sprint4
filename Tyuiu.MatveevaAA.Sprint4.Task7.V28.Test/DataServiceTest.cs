703696cccf164bacf66f7c01dd59e4d87ce7f4ea
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-11-23 02:39:57 +0500

    Task7

---
+++ Tyuiu.MatveevaAA.Sprint4.Task7.V28.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.MatveevaAA.Sprint4.Task7.V28.Lib;
+
+namespace Tyuiu.MatveevaAA.Sprint4.Task7.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int rows = 5;
+            int columns = 3;
+            int[,] mrex = new int[rows, columns];
+            string str = "623351179845632";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 4608;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MatveevaAA.Sprint4.Task7.V28/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.MatveevaAA.Sprint4.Task7.V28.Lib;
+
+namespace Tyuiu.MatveevaAA.Sprint4.Task7.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            Console.Title = "Спринт #4 | Выполнил: Матвеева А А | ПКТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:  Добавление к решению итоговых проектов по спринту                *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Матвеева А А | ПКТБ-25-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр 623351179845632. Преобразуйте её в    *");
+            Console.WriteLine("* матрицу 5 на 3 и подсчитайте произведение четных чисел.                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int rows = 5;
+            int cols = 3;
+            int[,] mtrx = new int[rows, cols];
+            int index = 0;
+            string str = "623351179845632";
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{str[index]}");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение чётных чисел = " + ds.Calculate(rows, cols, str));
+            Console.ReadKey();
+        }
+    }
+}
